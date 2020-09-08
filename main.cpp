6ce2b0f18ecb41fa7d0a940530ab0b2bc6ee7244
Stanley Hayford <hayfordstanley@gmail.com>
2020-09-08 11:26:51 +0000

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    //:: scope resolution operator which tells the compiler
+    //which namespace the object cout is from in this case std
+    std::cout << "Hello, Object-Oriented Programming!" << std::endl;
+    return 0;
+}
